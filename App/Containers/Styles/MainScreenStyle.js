ad7f38440e062fdaf4959683327743f5664a64a0
barelsh <barelshmu@gmail.com>
2018-07-07 11:51:09 +0300

    Added MainScreen
    changed colors

---
+++ App/Containers/Styles/MainScreenStyle.js
@@ -0,0 +1,33 @@
+import { StyleSheet } from 'react-native'
+import { ApplicationStyles, Metrics, Colors } from '../../Themes'
+
+export default StyleSheet.create({
+  ...ApplicationStyles.screen,
+  container: {
+    flex: 1,
+    backgroundColor: Colors.background,
+    // justifyContent: 'center'
+  },
+  row: {
+    flex: 1,
+    backgroundColor: Colors.facebook,
+    marginVertical: Metrics.smallMargin,
+    justifyContent: 'center'
+  },
+  boldLabel: {
+    fontWeight: 'bold',
+    alignSelf: 'center',
+    color: Colors.snow,
+    textAlign: 'center',
+    marginBottom: Metrics.smallMargin
+  },
+  label: {
+    textAlign: 'center',
+    color: Colors.snow
+  },
+  listContent: {
+    marginTop: Metrics.baseMargin,
+    width: '80%',
+    margin: '10%',
+  }
+})
