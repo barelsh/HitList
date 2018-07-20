0a09c62169adaeb3f49f98e8689a57add7a98e9c
barelsh <barelshmu@gmail.com>
2018-07-20 13:28:51 +0300

    Added List details read on API

--- App/Services/LocalStorageApi.js
+++ App/Services/LocalStorageApi.js
@@ -46,12 +46,60 @@ export async function setExampleLists(userId){
       [
         {id: 0, title: 'First List', description: 'First Description'},
         {id: 1, title: 'Second List', description: 'Second Description'},
-        {id: 2, title: 'Third List', description: 'Third Description'},
-        {id: 3, title: 'Fourth List', description: 'Fourth Description'},
       ])
+
+  await _storeData(`Lists:0:Members`,
+    [
+      {id:0, name: 'barel'},
+      {id:1, name: 'shmuel'},
+      {id:2, name: 'susu'},
+      {id:3, name: 'batata'},
+    ])
+  await _storeData(`Lists:0:Balances`,
+    [
+      {memberId: 0, balance: 100},
+      {memberId: 1, balance: -150},
+      {memberId: 2, balance: 50},
+      {memberId: 3, balance: 0},
+    ])
+
+  await _storeData(`Lists:1:Members`,
+    [
+      {id:0, name: 'barel'},
+      {id:1, name: 'boris'},
+    ])
+  await _storeData(`Lists:1:Balances`,
+    [
+      {memberId: 0, balance: 400},
+      {memberId: 1, balance: -400},
+    ])
+}
+
+export async function getListMembers(listId){
+  console.log('fetching...')
+  const data = await _retrieveData(`Lists:${listId}:Members`);
+  console.log('fetched! ' + JSON.stringify(data))
+  return data;
+}
+
+export async function getListBalances(listId){
+  console.log('fetching...')
+  const data = await _retrieveData(`Lists:${listId}:Balances`);
+  console.log('fetched! ' + JSON.stringify(data))
+  return data;
+}
+
+//TODO test
+export async function fetchListTransactions(listId) {
+  console.log('fetching...')
+  const data = await _retrieveData(`Lists:${listId}:Transactions`);
+  console.log('fetched! ' + JSON.stringify(data))
+  return data;
 }
 
 export default {
   fetchLists,
-  setExampleLists
+  setExampleLists,
+  getListBalances,
+  getListMembers
 }
