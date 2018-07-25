bba4e4b62eb2d7b1e08ed8dbd03ea59a70068205
barelsh <barelshmu@gmail.com>
2018-07-26 02:47:47 +0300

    * Added generateGuid util

--- App/Lib/utils.js
+++ App/Lib/utils.js
@@ -8,6 +8,17 @@ module.exports = {
     return array2.map(row =>              // loop through n items
       reducer(ix.get(row[key2]),     // get corresponding row from primary
         row));
+  },
+
+  generateGuid: () => {
+    function s4() {
+      return Math.floor((1 + Math.random()) * 0x10000)
+        .toString(16)
+        .substring(1);
+    }
+
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+           s4() + '-' + s4() + s4() + s4();
   }
 
 }
