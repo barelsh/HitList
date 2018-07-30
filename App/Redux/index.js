5e9555fdb91b400d76a9ad3f36284bd952002fd6
barelsh <barelshmu@gmail.com>
2018-07-30 21:08:35 +0300

    Added AddListRedux

--- App/Redux/index.js
+++ App/Redux/index.js
@@ -7,9 +7,10 @@ export const reducers = combineReducers({
   nav: require('./NavigationRedux').reducer,
   // github: require('./GithubRedux').reducer,
   // search: require('./SearchRedux').reducer,
+  addList: require('./AddListRedux').reducer,
   fetchLists: require('./FetchListsRedux').reducer,
   selectList: require('./SelectListRedux').reducer,
-  addTransaction: require('./AddTransactionRedux').reducer
+  addTransaction: require('./AddTransactionRedux').reducer,
 })
 
 export default () => {
